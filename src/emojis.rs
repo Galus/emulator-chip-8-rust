aeceb976f6c85795d5aa05d0ed2d0c31e64e13d2
Galus <mgalus@gmail.com>
2025-08-16 13:15:33 -0400

    Clean main

--- src/emojis.rs
+++ src/emojis.rs
@@ -1,2 +1,15 @@
 // In a separate file, e.g., emojis.rs
-pub static EMOJIS: &[&str] = &["🧨", "🖊️", "👁️", "🕹️", "🖥️", "🏃"];
+use phf::phf_map; // Static, Compile-time map.
+
+pub static EMOJIS: phf::Map<&'static str, &'static str> = phf_map! {
+    "dynamite" => "🧨",
+    "pen" => "🖊️",
+    "eye" => "👁️",
+    "joystick" => "🕹️",
+    "computer" => "🖥️",
+    "runner" => "🏃",
+    "handwave" => "👋",
+};
+
+// Old EMOJI[n] static
+// pub static EMOJIS: &[&str] = &["🧨", "🖊️", "👁️", "🕹️", "🖥️", "🏃"];
--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,65 @@
 // / / /_  _\      | (_| | (_| | | |_| \__ \            /_  _\/ /
 ///_/    \/         \__, |\__,_|_|\__,_|___/              \/ /_/
 //                  |___/
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, install, Result};
 
 mod emojis;
 mod emu;
 
-use emojis::EMOJIS; // Avoid Emoji Nightmares
+use emojis::EMOJIS as E; // Avoid Emoji Nightmares
 use emu::Emulator;
-use std::env;
-use tui_logger::{set_log_file, TuiLoggerFile, TuiLoggerLevelOutput};
+use std::env::args;
+use std::env::temp_dir;
+use std::fs::read;
+use tui_logger::{
+    init_logger, set_default_level, set_log_file, TuiLoggerFile, TuiLoggerLevelOutput,
+};
 
 #[macro_use]
 extern crate log;
 
-fn main() -> Result<()> {
-    color_eyre::install()?; // error hooks
-
-    tui_logger::init_logger(log::LevelFilter::Trace).unwrap();
-    tui_logger::set_default_level(log::LevelFilter::Trace);
-    let mut dir = env::temp_dir();
+fn setup_logging() -> Result<()> {
+    init_logger(log::LevelFilter::Trace).unwrap();
+    set_default_level(log::LevelFilter::Trace);
+    let mut dir = temp_dir();
     dir.push("chip8.log");
-    let file_options = TuiLoggerFile::new(dir.to_str().unwrap())
+    let dir_str = dir.to_str().ok_or(eyre!("Failed to get log file path"))?;
+    let file_options = TuiLoggerFile::new(dir_str)
         .output_level(Some(TuiLoggerLevelOutput::Abbreviated))
         .output_file(false)
-        .output_separater(':');
+        .output_separator(':');
     set_log_file(file_options);
-    debug!(target:"App", "Logging to {}", dir.to_str().unwrap());
+    debug!(target:"App", "Logging to {}", dir_str);
     debug!(target:"App", "Logging initialized");
 
-    println!("{} Initializing emulator", EMOJIS[0]);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    install()?; // error hooks
+    setup_logging()?;
+
+    println!("{} Initializing emulator", E["dynamite"]);
     let mut emu: Emulator = Emulator::new();
 
-    println!("\t{} Loading fonts into emulator...", EMOJIS[1]);
+    println!("\t{} Loading fonts into emulator...", E["pen"]);
     let _ = emu.load_font();
 
-    let rom_path: &str = "./roms/maze.ch8";
-    println!("\t{} Reading rom {}...", EMOJIS[2], rom_path);
-    let rom_data = std::fs::read(rom_path)?;
+    let rom_path: String = args()
+        .nth(1)
+        .ok_or(eyre!("Please provide a path to a ROM file"))?;
+
+    println!("\t{} Reading rom {}...", E["eye"], rom_path);
+    let rom_data = read(rom_path)?;
     emu.cpu.memory.rom = rom_data;
 
-    println!("\t{} Loading rom into emulator...", EMOJIS[3]);
+    println!("\t{} Loading rom into emulator...", E["joystick"]);
     let _ = emu.load_rom(); // clears emu.rom_buffer
 
-    println!("\t{} Initializing terminal...", EMOJIS[4]);
+    println!("\t{} Initializing terminal...", E["computer"]);
     let mut terminal = emu.cpu.memory.gpu.init()?;
 
-    println!("\t{} Running app...", EMOJIS[5]);
+    println!("\t{} Running app...", E["runner"]);
 
     loop {
         let _ = emu.cpu.fetch_opcode();
@@ -63,7 +76,7 @@ fn main() -> Result<()> {
 
         // Trying to figure out how to have above return a fn ptr
         // display
-        // input
+        // inpup
 
         // add a conditioal break
         break;
@@ -76,6 +89,6 @@ fn main() -> Result<()> {
         );
     }
 
-    println!("{} Exiting...", EMOJIS[6]);
+    println!("{} Exiting...", E["handwave"]);
     Ok(())
 }
